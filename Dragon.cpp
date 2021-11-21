f02ae069b00e5769e48c67950765122a90add091
Ryan Broemer <ryanbroemer@gmail.com>
2021-11-21 23:53:48 +0200

    GRRR

--- Dragon.cpp
+++ Dragon.cpp
@@ -5,6 +5,10 @@ using namespace std;
 #include "Dragon.h"
 #include "Payload.h"
 
+Dragon::Dragon() {
+	this->_payloadDescription = "Cargo to be sent to ISS.";
+}
+
 Dragon::Dragon(const Dragon& obj) {
 	this->_payloadDescription = obj._payloadDescription;
 }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "Dragon.h"
 using namespace std;
 int main() {
+    Payload * test = new Dragon();
+    cout<<test->getPayloadDescription()<<endl;
     return 0;
 }
