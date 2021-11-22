1bfd63d16f66edf9730cb684ed0ecbc2cef84216
Jonathan Enslin <jonathan.enslin112@gmail.com>
2021-11-22 13:43:35 +0200

    Removed unnecessary includes

--- mission_control/Builder/Builder.cpp
+++ mission_control/Builder/Builder.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 #include "Builder.h"
-#include "MissionControlStarbase.h"
-#include "RocketIterator.h"
-#include "Rocket.h"
+//#include "MissionControlStarbase.h" 	// Don't think needed
+//#include "RocketIterator.h" 			// Don't think needed
+//#include "Rocket.h"					// Don't think needed
 
 Rocket* Builder::buildRocket() {
 	throw "Not yet implemented";
