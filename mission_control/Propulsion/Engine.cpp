a58b46b97ff135bb24d5841e89d436d5a302b4e5
Adir Miller <adirmildog@gmail.com>
2021-11-23 00:00:33 +0200

    moved around the getters and setters

--- mission_control/Propulsion/Engine.cpp
+++ mission_control/Propulsion/Engine.cpp
@@ -36,3 +36,20 @@ void Engine::remove(Propulsion* aP) {
 	// DO NOTHING
 }
 
+void Engine::setLOXfuelLevel(int level){
+	// DO NOTHING
+}
+
+void Engine::setRP1fuelLevel(int level){
+	// DO NOTHING
+}
+
+int Engine::getLOXfuelLevel(){
+	// DO NOTHING
+	return 0;
+}
+
+int Engine::getRP1fuelLevel(){
+	// DO NOTHING
+	return 0;
+}
