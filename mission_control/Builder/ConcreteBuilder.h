569a984135b3129c8c727716cf06f7944de094c9
Jonathan Enslin <jonathan.enslin112@gmail.com>
2021-11-23 01:53:45 +0200

    Further Builder Implementation

--- mission_control/Builder/ConcreteBuilder.h
+++ mission_control/Builder/ConcreteBuilder.h
@@ -29,6 +29,7 @@ class ConcreteBuilder: public Builder
 	/// A integer value that counts the number of first stage boosters that have been added
 	private: int _numFirstStage;
 
+	/// An engine that is used for cloning
 	private: Engine _seaLevelMerlin;
 
 	/**
@@ -55,6 +56,33 @@ class ConcreteBuilder: public Builder
 	 * it will also automatically add an engine to the rocket
 	 */
 	public: void setSecondStage() override;
+
+	/**
+	 * @brief Sets the payload when using @b CARGO payload type
+	 * @param[in] payloadDescription - The payload description to give to the payload 
+	 * @details Sets the payload for payload type CARGO, will automatically create payload an initialise with parameters
+	 * @note - This functions is only to be used for setting the payload when @p payLoadType is @b CARGO . If it is not this type, the function will throw a @p string
+	 */
+    virtual void setPayload(string payloadDescription) override;
+  
+    /**
+     * @brief Set the Payload object when using @b STARLINK payload type
+     * @param[in] numSatellites The number of satellits to add to the chain of satellites (chain of responsibilty)
+	 * @details This function will create a starlink payload, for @b STARLINK payload type containing the number of satellites specified as a parameter
+	 * @note - Only to be used when payload type is @b STARLNK . If it is not this type the function will throw a @p string
+     */
+    virtual void setPayload(int numSatellites) override;
+    
+    /**
+     * @brief Set the Payload object when using @b CREW payload type
+     * 
+     * @param astronauts A vector of astronauts to add to the payload
+     * @param ranks A vector of the respective ranks of the astronauts
+	 * @details This will create a payload containing the passed astronauts, the vector of ranks should ideally be the same length as the
+	 * astronaut array, but differences in length will be ignored
+	 * @note - Only to be used when payload type is @b CREW . If it is not this type the function will throw a @p string
+     */
+    virtual void setPayload(vector<string> astronauts, vector<string> ranks) override;
 
 	/**
 	 * @brief Adds a first stage to the Rocket
