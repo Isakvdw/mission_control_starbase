3269a3e6a28286d213716b3db54ca45153476ce1
Jonathan Enslin <jonathan.enslin112@gmail.com>
2021-11-22 03:34:00 +0200

    Updated Observers + tested

--- Observer.cpp
+++ Observer.cpp
@@ -1,8 +1,14 @@
 #include <exception>
+#include <string>
 using namespace std;
 
 #include "Observer.h"
 #include "Propulsion.h"
+
+
+void Observer::setName(string aName) {
+	_observerName = aName;
+}
 
 void Observer::update() {
 	throw "Not yet implemented";
--- Observer.h
+++ Observer.h
@@ -1,4 +1,5 @@
 #include <exception>
+#include <string>
 using namespace std;
 
 #ifndef __Observer_h__
@@ -9,8 +10,21 @@ using namespace std;
 class Propulsion;
 class Observer;
 
+/**
+ * @brief A Booster observer class
+ * @author Jonathan Enslin - u19103345
+ */
 class Observer
 {
+	/// Name of the observer
+	protected: string _observerName;
+
+	/**
+	 * @brief Sets the name of the observer
+	 * @param[in] name Name to be assigned
+	 */
+	public: void setName(string name);
+
 	public: Propulsion* _unnamed_Propulsion_;
 
 	public: virtual void update() = 0;
