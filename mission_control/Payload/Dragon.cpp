6e0e514ea4edebc61a966fe27c216c5c14db10f8
Ryan Broemer <ryanbroemer@gmail.com>
2021-11-22 14:26:53 +0200

    Added Doxygen to Payload hierarchy

--- mission_control/Payload/Dragon.cpp
+++ mission_control/Payload/Dragon.cpp
@@ -6,19 +6,36 @@ using namespace std;
 
 #include "Dragon.h"
 #include "Payload.h"
-
+/**
+ * @brief Construct a new Dragon:: Dragon object
+ * 
+ */
 Dragon::Dragon() {
 	this->_payloadDescription = "Cargo to be sent to ISS!";
 }
 
+/**
+ * @brief Construct a new Dragon:: Dragon object
+ * 
+ * @param obj 
+ */
 Dragon::Dragon(const Dragon& obj) {
 	this->_payloadDescription = obj._payloadDescription;
 }
 
+/**
+ * @brief Gets the description of the Dragon payload as a string.
+ * 
+ * @return string 
+ */
 string Dragon::getPayloadDescription() {
 	return _payloadDescription;
 }
 
+/**
+ * @brief Displays the description of the Dragon payload to the user.
+ * 
+ */
 void Dragon::printPayload() {
 	cout<<getPayloadDescription()<<endl;
 }
--- mission_control/Payload/Starlink.cpp
+++ mission_control/Payload/Starlink.cpp
@@ -8,11 +8,21 @@ using namespace std;
 #include "SatelliteLauncher.h"
 #include "Payload.h" 
 
+/**
+ * @brief Construct a new Starlink:: Starlink object
+ * 
+ */
 Starlink::Starlink() {
 	this->_payloadDescription = "Cargo of satellites to be released into orbit!";
 	this->_satellites = NULL;
 }
 
+/**
+ * @brief Simulates the the ejection by calling the head Satellite
+ * to Handle the request which goes down the chain deleting and
+ * detaching satellites.
+ * 
+ */
 void Starlink::LaunchAllSatellites() {
 	this->printPayload();
 	if (this->_satellites != NULL) {
@@ -28,6 +38,11 @@ void Starlink::LaunchAllSatellites() {
 	}
 }
 
+/**
+ * @brief Add satellite to the chain.
+ * 
+ * @param Sat 
+ */
 void Starlink::addSat(SatelliteLauncher* Sat) {
 	if (this->_satellites == NULL)
 	{
@@ -39,6 +54,11 @@ void Starlink::addSat(SatelliteLauncher* Sat) {
 	}
 }
 
+/**
+ * @brief Gets the Starlink payload as a string.
+ * 
+ * @return string 
+ */
 string Starlink::getPayloadDescription() {
 	string out = "";
 	if (_satellites == NULL) {
@@ -53,6 +73,10 @@ string Starlink::getPayloadDescription() {
 	return out;
 }
 
+/**
+ * @brief Displays the Starlink payload to the user.
+ * 
+ */
 void Starlink::printPayload() {
 	cout<<getPayloadDescription()<<endl;
 }
