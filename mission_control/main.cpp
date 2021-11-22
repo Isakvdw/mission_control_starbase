c82f3bfa2905b82ecab1c2428ecfb26df4ef9c0d
Jonathan Enslin <jonathan.enslin112@gmail.com>
2021-11-22 15:55:56 +0200

    Makefile minor change

--- mission_control/main.cpp
+++ mission_control/main.cpp
@@ -51,16 +51,16 @@ int main() {
     sStage->notify();
 
     for (int i = 1; i <= 10; i++) {
-        fHeavy->setLOXfuelLevel(fHeavy->getLOXfuelLevel() - i*5);
-        fHeavy->setRP1fuelLevel(fHeavy->getRP1fuelLevel() - i*11);
+        fHeavy->setLOXfuelLevel(fHeavy->getLOXfuelLevel() - 5);
+        fHeavy->setRP1fuelLevel(fHeavy->getRP1fuelLevel() - 11);
         fHeavy->notify();
 
-        f->setLOXfuelLevel(f->getLOXfuelLevel() - i*10);
-        f->setRP1fuelLevel(f->getRP1fuelLevel() - i*5);
+        f->setLOXfuelLevel(f->getLOXfuelLevel() - 10);
+        f->setRP1fuelLevel(f->getRP1fuelLevel() - 5);
         f->notify();
 
-        sStage->setLOXfuelLevel(sStage->getLOXfuelLevel() - i*13);
-        sStage->setRP1fuelLevel(sStage->getRP1fuelLevel() - i*10);
+        sStage->setLOXfuelLevel(sStage->getLOXfuelLevel() - 13);
+        sStage->setRP1fuelLevel(sStage->getRP1fuelLevel() - 10);
         f->notify();
     }
 
