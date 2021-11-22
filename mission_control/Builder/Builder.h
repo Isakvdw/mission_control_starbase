f729363be3be2d8c7a278e379d16e00743e55042
Jonathan Enslin <jonathan.enslin112@gmail.com>
2021-11-22 17:47:00 +0200

    Builder.h modified

--- mission_control/Builder/Builder.h
+++ mission_control/Builder/Builder.h
@@ -5,18 +5,49 @@ using namespace std;
 #ifndef __Builder_h__
 #define __Builder_h__
 
-//#include "../MissionControlStarbase.h"
 #include "../Rocket.h"
+#include "../Propulsion/Booster.h"
 
+/**
+ * @brief Interface for a basic rocket builder class
+ */
 class Builder
 {
+	/// The type of the rocket
 	private: string _rocketType;
 
+	/**
+	 * @brief This methods returns the rocket that was built
+	 * @return A pointer to the rocket that was built
+	 * @details see the children class for implementation details
+	 */
 	public: virtual Rocket* buildRocket() = 0;
 
+	/**
+	 * @brief Getter for the rocket type
+	 * @return @p _rocketType - A string that is equal to the rockets type
+	 */
 	public: string getRocketType();
 
+	/**
+	 * @brief Setter for the rocket type
+	 * @param aRocketType The type of the rocket
+	 */
 	public: void setRocketType(string aRocketType);
+
+	/**
+	 * @brief Sets the second stage of the rocket
+	 * @param[in] secondStage - The second stage to be added to the rocket
+	 * @details See children classess for implementation details
+	 */
+	public: virtual void setSecondStage(Booster* secondStage)=0;
+
+	/**
+	 * @brief Adder for the first stage of the rocket
+	 * @param firstStage - The first stage to be added to the rocket
+	 * @details See the the children classes for implementation details
+	 */
+	public: virtual void addFirstStage(Booster* firstStage)=0;
 };
 
 #endif
