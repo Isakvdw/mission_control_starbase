ad441b2601ba585901968174e5fc789b8f3cdfe4
Isak <u20468203@tuks.co.za>
2021-11-23 04:49:55 +0200

    caretaker documentation

--- mission_control/Storage/Caretaker.h
+++ mission_control/Storage/Caretaker.h
@@ -17,10 +17,32 @@ class Caretaker {
  public:  
   Caretaker();
   ~Caretaker();
+  /**
+	 * @brief Creates an iterator that is used to traverse the memento list
+	 * @return return the iterator
+	 */
   Iterator* createIterator();
+
+  /**
+	 * @brief Adds a rocket to the memento list
+	 * @param rockMem a pointer to a RocketMemento to be added to memento list
+	 */
   void storeRocket(RocketMemento* rockMem);
+
+  /**
+	 * @brief Adds in a batch of rockets
+	 * @param aBatch a Batch t of rockets that will server as the new memento list
+	 */
   void batchStore(RocketAggregate* aBatch);
+
+  void batchRun();
+
+  /**
+	 * @brief Returns lastly stored rocket's state
+	 * @return returns lastly added RocketMemento
+	 */
   RocketMemento* RestoreLast();
+  
   void removeLast();
 };
 
